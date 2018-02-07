aa3a42304ebdd1f859ed76ad46cb2ebfaebe9100
Unknown <pagejakob@gmail.com>
2018-02-07 12:40:35 +1100

    Completed
    Added GoogleMaps, CSS tweaks and more

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,12 +5,12 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather(city){
-    const url = `${ROOT_URL}&q=${city},au`;
-    const request = axios.get(url);
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},au`;
+  const request = axios.get(url);
 
-    return{
-        type: FETCH_WEATHER,
-        payload: request
-    };
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
 }
